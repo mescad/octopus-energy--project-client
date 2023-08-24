779a6e9aebb18ad32b2e07aad02f7776f81b0959
Dumitru Mesca <dumitrumesca@gmail.com>
2023-08-24 17:52:43 +0100

    initial build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import "./App.scss";
+import "./styles/partials/_mixins.scss";
+import "./styles/partials/_resets.scss";
+import "./styles/partials/_typography.scss";
+import "./styles/partials/_variables.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
