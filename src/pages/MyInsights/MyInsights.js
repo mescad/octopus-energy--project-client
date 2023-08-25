2d7ef31f256386da9bc9bfeb6b66652eae68e134
Patrick Oconnor <ptoceng@gmail.com>
2023-08-25 10:36:21 +0100

    basic info section

--- src/pages/MyInsights/MyInsights.js
+++ src/pages/MyInsights/MyInsights.js
@@ -2,15 +2,16 @@ import "./MyInsights.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import Charts from "../../components/Chart/Chart";
 import Footer from "../../components/Footer/Footer";
-
+import Infosection from "../../components/Infosection/Infosction";
 const MyInsights = () => {
   return (
     <>
-  <Navbar />
-  <Charts /> 
-  <Footer />
-  </>
-  )
+      <Navbar />
+      <Charts />
+      <Infosection />
+      <Footer />
+    </>
+  );
 };
 
 export default MyInsights;
