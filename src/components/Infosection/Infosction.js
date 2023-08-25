c23da8546c206c431673b1b5f88ef2a98e1a5028
Patrick Oconnor <ptoceng@gmail.com>
2023-08-25 13:18:14 +0100

    info section styling completed

--- src/components/Infosection/Infosction.js
+++ src/components/Infosection/Infosction.js
@@ -2,53 +2,57 @@ import "./Infosection.scss";
 import { Link } from "react-router-dom";
 import arrow from "../../assets/images/icons/entypo_chevron-left.svg";
 import fire from "../../assets/images/icons/fire.svg";
+import elip from "../../assets/images/icons/Ellipses.png";
 
 const Infosection = () => {
   return (
     <section className="infosection">
       <div className="infosection__horozontalbox">
         <div className="infosection__horozontalbox-textcontainer">
-          <h3 className="infosection__horozontalbox-title">Energy Usage</h3>
-          <h2 className="infosection__horozontalbox-info">Your Breakdown</h2>
-        </div>
-        <div className="infosection__horozontalbox-arrowcontainer">
-          <img className="infosection__horozontalbox-arrow" src={arrow} />
-        </div>
-      </div>
-      <div className="infosection__vertbox-container">
-        <div className="infosection__vertbox">
-          <h3 className="infosection__vertbox-title">My Trends</h3>
-          <p className="infosection__vertbox-info">3% ⬇️ </p>
-          <p className="infosection__vertbox-summary">
-            You've reduced your carbon footprint by 3% againts last month. Nice
-            one!
-          </p>
-        </div>
-        <div className="infosection__vertbox">
-          <h3 className="infosection__vertbox-title">Recommendations</h3>
+          <h3 className="infosection__horozontalbox-title"><b>Eco Tips</b></h3>
           <ul className="infosection__vertbox-reclist">
             <li className="infosection__vertbox-reclistitem">
               <img className="infosection__vertbox-reclist--icon" src={fire} />
-              <p
-                class="infosection__vertbox-reclist-p"
-              >
-                Charge car overnight
-              </p>
+              <p class="infosection__vertbox-reclist-p">Charge car overnight</p>
             </li>
             <li className="infosection__vertbox-reclistitem">
               <img className="infosection__vertbox-reclist--icon" src={fire} />
-              <p
-                class="infosection__vertbox-reclist-p"
-              >
+              <p class="infosection__vertbox-reclist-p">
                 Wash Clothes at 30 degrees
               </p>
             </li>
             <li className="infosection__vertbox-reclistitem">
               <img className="infosection__vertbox-reclist--icon" src={fire} />
-              <p
-                class="infosection__vertbox-reclist-p">Try out Agile Octopus</p>
+              <p class="infosection__vertbox-reclist-p">
+                Try out <b>Agile Octopus</b>
+              </p>
             </li>
           </ul>
+          <div className="infosection__vertbox-reclist-imagecontainer">
+          <img className="infosection__vertbox-reclist-icon" src={elip} />
+          </div>
+        </div>
+      </div>
+      <div className="infosection__vertbox-container">
+        <div className="infosection__vertbox">
+          <h3 className="infosection__vertbox-title">My Trends</h3>
+          <p className="infosection__vertbox-info">+3%</p>
+          <p className="infosection__vertbox-summary">
+            increase in Eco Usage since last month
+          </p>
+          <div className="infosection__vertbox-reclist-imagecontainer2">
+          <img className="infosection__vertbox-reclist-icon2" src={elip} />
+          </div>
+        </div>
+        <div className="infosection__vertbox">
+          <h3 className="infosection__vertbox-title">My Rank</h3>
+          <p className="infosection__vertbox-info">16th</p>
+          <p className="infosection__vertbox-summary">
+            out of 100 users in your community
+          </p>
+          <div className="infosection__vertbox-reclist-imagecontainer2">
+          <img className="infosection__vertbox-reclist-icon2" src={elip} />
+          </div>
         </div>
       </div>
     </section>
